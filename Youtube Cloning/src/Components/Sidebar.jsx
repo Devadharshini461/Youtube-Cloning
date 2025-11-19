d56f2c3844b54a9dbadb611547ec2778b62a1aa4
devadharshini461 <devadharshini.s461@gmail.com>
2025-11-19 11:10:57 +0530

    added profile

--- Youtube Cloning/src/Components/Sidebar.jsx
+++ Youtube Cloning/src/Components/Sidebar.jsx
@@ -17,7 +17,7 @@ function Sidebar() {
       <h3>You</h3>
       <br></br>
 
-      <div className='item' onClick={()=>{navigate('/History')}}><i className="fa-solid fa-clock-rotate-left" style={{color: "#000000"}}></i><span>History</span></div>
+      <div className='item'><i className="fa-solid fa-clock-rotate-left" style={{color: "#000000"}}></i><span>History</span></div>
       <div className='item'><i className="fa-solid fa-graduation-cap" style={{color: "#000000",}}></i><span>Your Courses</span></div>
       <div className='item'><i className="fa-regular fa-clock" style={{color: "#000000",}} ></i><span>Watch Later</span></div>
       <div className='item'><i className="fa-regular fa-thumbs-up" style={{color: "#000000",}} ></i><span>Liked Videos</span></div>
